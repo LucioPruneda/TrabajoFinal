ce13bfc815a6650995c9fd5f0c59fc27f5e999cc
LucioPruneda <lucio_pruneda@hotmail.com>
2021-10-22 11:43:07 -0300

    Segundo comentario, se agregaron dos archivos

--- nota.py
+++ nota.py
@@ -1,28 +1,17 @@
-#! /usr/bin/python3
-import datetime
-
-# Guardamos en una variable la próxima id disponible:
-ultima_id = 0
-
-
 class Nota:
     '''Representa una nota en el anotador. Tiene texto, etiquetas (separadas 
     por espacios) y se puede buscar por texto'''
-
-    def __init__(self, texto, etiquetas=''):
+    def __init__(self, texto, etiquetas='', id_nota=None):
         '''Inicializa la nota con un texto, y opcionalmente, con etiquetas 
         separadas por espacios. Automáticamente define fecha de creación e id'''
         self.texto = texto
         self.etiquetas = etiquetas
-        self.fecha_creacion = datetime.date.today()
-        global ultima_id
-        ultima_id += 1
-        self.id = ultima_id
+        self.id = id_nota
 
     def coincide(self, filtro):
         '''Determina si la nota coincide con el filtro de búsqueda. Retorna 
         True si es así y False de lo contrario
-
+        
         Busca tanto en el texto como en las etiquetas y distingue mayúsculas de
         minúsculas '''
 
@@ -30,10 +19,3 @@ class Nota:
             return True
         else:
             return False
-
-        # TODO: Construir este método
-        # Recordatorio para buscar en una cadena:
-        # if {subcadena} in {cadena}:
-        # Ej:
-        # if 'ten' in 'entretenido':  ---> sale por True
-        # if 'blabla' in 'entretenido': ---> sale por False
